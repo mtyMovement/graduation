88a1bd8c3d085513161c920a0a8c1eabdc41691f
maoty <846319617@qq.com>
2019-04-19 16:28:11 +0800

    first commit

---
+++ src/main/java/com/graduation/jaguar/JaguarApplication.java
@@ -0,0 +1,13 @@
+package com.graduation.jaguar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaguarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaguarApplication.class, args);
+    }
+
+}
